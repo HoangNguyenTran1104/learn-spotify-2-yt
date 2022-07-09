20227bec75b32ef2fdc76e30718b2e48aa8bd558
tran.hoang <hoang.nt@cyberlogitec.com>
2022-07-09 12:33:19 +0700

    update learning 1h25m54s

--- spotify-2-yt/pages/api/auth/[...nextauth].js
+++ spotify-2-yt/pages/api/auth/[...nextauth].js
@@ -1,6 +1,31 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
-import {LOGIN_URL} from "../../../lib/spotify"
+import spotifyAPI,{LOGIN_URL} from "../../../lib/spotify"
+
+async function refreshAccessToken(token){
+  try{
+    spotifyAPI.setAccessToken(token.accessToken)
+    spotifyAPI.setRefreshToken(token.refreshToken)
+
+    const {body: refreshedToken} = await spotifyAPI.refreshAccessToken()
+    console.log("REFRESHED TOKEN IS ", refreshedToken)
+    return{
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshToken.expires_in * 1000, // = 1 hour 3600 return from Spotify API
+      refreshToken: refreshedToken.refresh_token ?? token.refresh_token // replace if new one came back else fall back to old refresh token
+    }
+
+  }
+  catch(error){
+    console.error(error)
+    return{
+      ... token, 
+      error: "RefrshAccessToken"
+    };
+
+  }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -32,10 +57,21 @@ export default NextAuth({
 
       // Return previous token if the access token has not expired
       if(Date.now() < token.accessTokenExpires){
-        return token;
+        console.log("EXISTING ACCESS TOKEN IS VALID")
+        return token
       }
 
-      //Access token has
+      //Access token has expired, so we need to refresh it...
+      console.log("ACCESS TOKEN HAS EXPIRED, REFRESHING...");
+      return await refreshAccessToken(token)
+    },
+
+    async session({ session,token}){
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+       session.user.username = token.username;
+
+      return session
     }
   }
 
--- spotify-2-yt/pages/login.js
+++ spotify-2-yt/pages/login.js
@@ -1,10 +1,21 @@
+import {getProviders, signIn} from 'next-auth/react'
 
-function login() {
+function Login(providers) {
   return (
     <div>
-       <h1>This is login page </h1>
+       <img className="w-52 mb-5" src="/spotify-logo.png" alt="" />
     </div>
   )
 }
 
-export default login
+export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return{
+    props:{
+      providers
+    }
+  }
+}
+
